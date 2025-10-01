67bfb83a345cc41a04837f6845e0521bde938aa0
CHHORN Seyha <senjihachiro@gmail.com>
2025-10-01 14:12:18 +0700

    old commit

--- src/main/java/com/khrd/product_service/model/dto/request/ProductRequest.java
+++ src/main/java/com/khrd/product_service/model/dto/request/ProductRequest.java
@@ -1,9 +1,11 @@
 package com.khrd.product_service.model.dto.request;
 
-import com.khrd.product_service.model.entity.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.UUID;
